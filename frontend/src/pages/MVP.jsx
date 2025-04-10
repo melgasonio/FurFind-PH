a85ba49e62ebdd364f0e83a7515bdd5864ba50ee
Melga <soniomelga@gmail.com>
2025-04-11 03:06:49 +0800

    Start building component for MVP page

---
+++ frontend/src/pages/MVP.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import darak from "../assets/mvp/darak.webp"
+
+const MVP = () => {
+  return (
+    <div>
+        <img src={darak} />
+        <div>Oh. Hello there!</div>
+        <p>This isn’t a fully working website — it’s an MVP (Minimum Viable Portfolio). Built to showcase a dev’s skills (and desperation, let’s be honest). Nothing’s real here except her need for a job.</p>
+    </div>
+  )
+}
+
+export default MVP
